8fc5abbbb8459d589d5d12471379676cf1678840
Kian Woo <kian.woo@gmail.com>
2022-05-08 20:36:06 +1000

    feat: update rich text asset handling

--- components/rich-text-asset.js
+++ components/rich-text-asset.js
@@ -1,10 +1,20 @@
-import Image from 'next/image'
+import ContentfulImage from './contentful-image'
 
 export default function RichTextAsset({ id, assets }) {
   const asset = assets?.find((asset) => asset.sys.id === id)
 
   if (asset?.url) {
-    return <Image src={asset.url} layout="fill" alt={asset.description} />
+    console.log(asset)
+    const url = asset.url
+    if (url.endsWith('.pdf') || url.endsWith('.mp3')) {
+      return <a className="font-bold underline underline-offset-2" href={asset.url}>{asset.description}</a>
+    }    
+    return (
+      <div className="w-full h-[100vw] sm:h-96 relative">
+        <ContentfulImage src={asset.url} objectFit="cover" layout="fill" alt={asset.description} />
+      </div>
+      
+    )
   }
 
   return null
