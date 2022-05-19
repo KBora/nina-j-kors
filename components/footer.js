c205be1522e5dcdd4e564b29663930e607b13c4f
Kian Woo <kian.woo@gmail.com>
2022-05-19 22:31:05 +1000

    feat: footer placeholder

--- components/footer.js
+++ components/footer.js
@@ -6,8 +6,7 @@ export default function Footer() {
     <footer className="bg-accent-1 border-t border-accent-2">
       <Container>
         <div className="py-28 flex flex-col lg:flex-row items-center">
-          Gruppo Telegram: https://t.me/ninalegge
-          Instagram
+          Footer to do: eg socials, contact details
         </div>
       </Container>
     </footer>
