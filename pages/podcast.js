3c8156ed3f4675afacdcf49a6ed78165a1cd0bbf
Kian Woo <kian.woo@gmail.com>
2023-06-18 19:16:11 +1000

    feat: update podcast image

--- pages/podcast.js
+++ pages/podcast.js
@@ -49,7 +49,7 @@ export default function Index() {
               </a>
             </div>
 
-            <Image src="/submarin.jpg" width="2000" height="1341"></Image>
+            <Image src="/submarine.jpg" width="2000" height="1341"></Image>
 
           </div>
           
